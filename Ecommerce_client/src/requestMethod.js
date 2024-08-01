4b5dde32d6ce8f84cd3247c4e69137d12fdfc44b
YashJha26 <142669810+YashJha26@users.noreply.github.com>
2024-08-01 18:43:41 +0530

    Update requestMethod.js

--- Ecommerce_client/src/requestMethod.js
+++ Ecommerce_client/src/requestMethod.js
@@ -1,22 +1,15 @@
 //FInal Version
 import axios from "axios";
-import { useSelector } from "react-redux";
 const base_URL="https://ecommerce-website-backend-5fvc.onrender.com/api"
 //const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2YTBmNDY0NzdkYWI3ODJiZWYxMjk0ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcyMjIzMDQxOCwiZXhwIjoxNzIyMzE2ODE4fQ.oi_O718JX1b044WKP2cL3rYc2PDjCyVNCiqQnwhcYrA"
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const token = currentUser?.accessToken;
 export const publicRequest = axios.create({
     baseURL:base_URL,
-});
+})
 
 export const userRequest = axios.create({
     baseURL:base_URL,
-    //header:{Authorization:`Bearer ${token}`}
-});
-
-userRequest.interceptors.request.use((config) => {
-  const user = useSelector((state) => state.user.currentUser);
-  const token = user?.accessToken; // Optional chaining to handle null/undefined
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+    header:{Authorization:`Bearer ${token}`}
+})
