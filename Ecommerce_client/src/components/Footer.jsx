cf5b1f8e6a2c68e6a38b07b1522ddca90a0f5b87
YashJha26 <142669810+YashJha26@users.noreply.github.com>
2024-10-30 08:31:57 +0530

    Update Footer.jsx, logo change

--- Ecommerce_client/src/components/Footer.jsx
+++ Ecommerce_client/src/components/Footer.jsx
@@ -72,7 +72,7 @@ const Footer = () => {
   return (
     <Container>
        <Left>
-        <Logo>LAMA.</Logo>
+        <Logo>WARDROBE.</Logo>
         <Desc>
           There are many variations of passages of Lorem Ipsum available, but
           the majority have suffered alteration in some form, by injected
