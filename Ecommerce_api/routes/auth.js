776801699edd6cdc71fd7cdd1b22d5651b8c096c
YashJha26 <142669810+YashJha26@users.noreply.github.com>
2024-10-30 08:03:01 +0530

    Update auth.js

--- Ecommerce_api/routes/auth.js
+++ Ecommerce_api/routes/auth.js
@@ -49,7 +49,7 @@ router.post("/login", async (req,res)=>{
         )
           res.status(200).json({user,accessToken});
     } catch (error) {
-        res.status(500).json(err);
+        res.status(500).json(error);
     }
 });
 
